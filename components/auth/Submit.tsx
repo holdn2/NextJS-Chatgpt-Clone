f4c7a6bd958f3e865c16cf933491485622283721
holdn2 <yoocy01@gmail.com>
2025-04-21 22:53:04 +0900

    fix : pending 시 버튼 비활성화

--- components/auth/Submit.tsx
+++ components/auth/Submit.tsx
@@ -1,9 +1,12 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, useActionState } from "react";
 import { Button } from "../ui/button";
+import { login } from "@/actions/login";
 
 export function Submit({ children, ...others }: ComponentProps<typeof Button>) {
+  const [error, action, isPending] = useActionState(login, undefined);
+
   return (
-    <Button type="submit" {...others}>
+    <Button disabled={isPending} type="submit" {...others}>
       {children}
     </Button>
   );
